0048b3aba4f97657939fa4e15a986a4fcaa46db5
Debdut Karmakar <iawaiponly@gmail.com>
2016-08-05 14:46:27 -0400

    Fixed Bugs

--- src/my_string.c
+++ src/my_string.c
@@ -3,7 +3,7 @@
 char *ptr = NULL;
 int counter = 0;
 
-int strlen (char *str)
+int m_strlen (char *str)
 /*Returns the length of the char string*/
 {
     char* ptr = str;
@@ -15,40 +15,43 @@ int strlen (char *str)
     return (ptr - str);
 }
 
-char *strcp_indx (char *str, int start, int stop)
+char *m_strcp_indx (char *str, int start, int stop)
 /*Creates and Returns a substring of string using indices in the given string*/
 {
-    char *str = (char *) calloc (1, sizeof (char));
+    unsigned int diff_indx = stop - start;
+    char *ptr = (char *) calloc (1, sizeof (char));
+    char *ptr2, *ptr3, *cp_str;
 
     if (start >= stop)
     {
-        return str;
+        return ptr;
     }
 
-    free (str);
+    free (ptr);
 
-    unsigned int diff_indx = stop - start;
-    char *str = (char *) calloc (diff_indx, sizeof (char));
-    char *ptr = str;
+    diff_indx = stop - start;
+    ptr = (char *) calloc (diff_indx, sizeof (char));
+    ptr2 = ptr;
 
-    while ((*(ptr++) != '\0') && (ptr - str < start))
+    while ((*(ptr2++) != '\0') && (ptr2 - ptr < start))
     {
     }
 
-    char *ptr2 = ptr;
-    char *cp_str = str;
+    ptr3 = ptr2;
+    cp_str = ptr;
 
-    while (((*str++ = *ptr++) != '\0') && (ptr-ptr2 < diff_indx))
+    while (((*ptr++ = *ptr2++) != '\0') && (ptr2-ptr3 < diff_indx))
     {
     }
 
     return cp_str;
 }
 
-char *strcp (char* start, char* stop)
+char *m_strcp (char* start, char* stop)
 /*Creates and Returns a substring of string*/
 {
     char *str = (char *) calloc (1, sizeof (char));
+    char *ptr;
 
     if (start >= stop)
     {
@@ -58,7 +61,7 @@ char *strcp (char* start, char* stop)
     free (str);
 
     str = (char *) malloc ((stop - start + 1) * sizeof (char));
-    char *ptr = str;
+    ptr = str;
 
     while (start != stop)
     {
@@ -69,7 +72,7 @@ char *strcp (char* start, char* stop)
     return str;
 }
 
-int str_match_begin (char *str, char *target)
+int m_str_match_begin (char *str, char *target)
 /*Boolean function returns 1 target is in begining of str, 0 otherwise*/
 {
     while ((*str != '\0') && (*str == *target))
@@ -81,9 +84,11 @@ int str_match_begin (char *str, char *target)
     return (*target == '\0');
 }
 
-char *str_find (char *str, char *target)
+char *m_str_find (char *str, char *target)
 /*Returns the pointer to begining of target string in str string*/
 {
+    char *ptr;
+
     if ((target == NULL) || (*target == '\0'))
     {
         return str;
@@ -93,24 +98,26 @@ char *str_find (char *str, char *target)
         return NULL;
     }
 
-    char *ptr = str;
+    ptr = str;
 
-    while ((*ptr != '\0') && !str_find_begin (ptr, target))
+    while ((*ptr != '\0') && !m_str_find_begin (ptr, target))
     {
         ptr++;
     }
 
     if (*ptr == '\0')
     {
-        return -1;
+        return NULL;
     }
 
-    return *ptr;
+    return ptr;
 }
 
-int str_find_indx (char *str, char *target)
+int m_str_find_indx (char *str, char *target)
 /*Returns the index of begining of target string in str string*/
 {
+    char *ptr;
+
     if ((target == NULL) || (*target == '\0'))
     {
         return 0;
@@ -120,9 +127,9 @@ int str_find_indx (char *str, char *target)
         return -1;
     }
 
-    char *ptr = str;
+    ptr = str;
 
-    while ((*ptr != '\0') && !str_find_begin (ptr, target))
+    while ((*ptr != '\0') && !m_str_find_begin (ptr, target))
     {
         ptr++;
     }
@@ -135,23 +142,25 @@ int str_find_indx (char *str, char *target)
     return (*ptr - *str);
 }
 
-char *str_find_next (char *str, char *target, unsigned int next)
+char *m_str_find_next (char *str, char *target, unsigned int next)
 /*Find the next'th target in str string*/
 {
     if (next == 0)
     {
-        return str_find (str, target);
+        return m_str_find (str, target);
     }
 
-    ptr = str_find (str, target);
+    ptr = m_str_find (str, target);
 
-    return str_find_next(ptr+1, target, next-1);
+    return m_str_find_next(ptr+1, target, next-1);
 
 }
 
-char *str_split (char *str, const char *delim)
+char *m_str_split (char *str, char *delim)
 /*Gets the next token after delimiter if str is NULL, otherwise gets first token in str*/
 {
+    char *start_ptr, *stop_ptr;
+
     if ((str == NULL) && (ptr == NULL))
     {
         return NULL;
@@ -162,15 +171,15 @@ char *str_split (char *str, const char *delim)
         ptr = str;
     }
 
-    if (str_find (ptr, delim) == NULL)
+    if (m_str_find (ptr, delim) == NULL)
     {
         ptr = NULL;
         return NULL;
     }
 
-    char *start_ptr = str_find (ptr, delim);
-    start_ptr += len (delim);
-    char *stop_ptr = str_find (start_ptr, delim);
+    start_ptr = m_str_find (ptr, delim);
+    start_ptr += m_strlen (delim);
+    stop_ptr = m_str_find (start_ptr, delim);
     ptr = stop_ptr;
-    return strcp(start_ptr, stop_ptr);
+    return m_strcp(start_ptr, stop_ptr);
 }
--- src/my_string.h
+++ src/my_string.h
@@ -3,11 +3,13 @@
 
 #include <stdlib.h>
 
-int strlen (char *str);
-char *strcp_indx (char *str, int start, int stop);
-char *strcp (char* start, char* stop);
-int str_find_begin (char *str, char *target);
-char *str_find (char *str, char *target);
-int str_find_indx (char *str, char *target);
-char *str_find_next (char *str, char *target, unsigned int next);
-char *str_split (char *str, const char *delim);
+int m_strlen (char *str);
+char *m_strcp_indx (char *str, int start, int stop);
+char *m_strcp (char* start, char* stop);
+int m_str_find_begin (char *str, char *target);
+char *m_str_find (char *str, char *target);
+int m_str_find_indx (char *str, char *target);
+char *m_str_find_next (char *str, char *target, unsigned int next);
+char *m_str_split (char *str, char *delim);
+
+#endif
